f4ee12850e05820933e158de444f5f2acf07d64f
石一龙 <yilong.shi@hand-china.com>
2017-02-10 17:28:04 +0800

    admin 中 增加删除的UI。

--- js/admin/adminCtrl.js
+++ js/admin/adminCtrl.js
@@ -1,6 +1,71 @@
 /**
  * Created by applesyl on 2017/2/10.
  */
-App.controller('adminCtrl',function($scope,$stateParams){
+App.controller('adminCtrl',function($scope,$stateParams,ngDialog){
 
+
+    //添加教师
+    $scope.addTeacher = function(){
+        ngDialog.open({
+            width: 600,
+            template: 'code/admin/addTeacher.html',
+            className: 'ngdialog-theme-default',
+            closeByDocument : false,
+            closeByEscape : false,
+            controller: function($scope) {
+                $scope.closeThisDialog = function() {
+                    ngDialog.close(); //关闭弹窗
+                };
+            }
+        })
+    };
+
+    //修改密码
+    $scope.cheangePassword = function(){
+        ngDialog.open({
+            width: 600,
+            template: 'code/common/cheangePassword.html',
+            className: 'ngdialog-theme-default',
+            closeByDocument : false,
+            closeByEscape : false,
+            controller: function($scope) {
+                $scope.closeThisDialog = function() {
+                    ngDialog.close(); //关闭弹窗
+                };
+            }
+        })
+    };
+
+    //查看信息
+    $scope.seeTeacherDetails = function(){
+        ngDialog.open({
+            width: 600,
+            template: 'code/admin/seeTeacherDetails.html',
+            className: 'ngdialog-theme-default',
+            closeByDocument : false,
+            closeByEscape : false,
+            controller: function($scope) {
+                $scope.closeThisDialog = function() {
+                    ngDialog.close(); //关闭弹窗
+                };
+            }
+        })
+    };
+
+    //删除
+    $scope.removerTeacher = function(){
+        ngDialog.open({
+            width: 360,
+            template: 'code/common/remove.html',
+            className: 'ngdialog-theme-default',
+            showClose : false,
+            closeByDocument : false,
+            closeByEscape : false,
+            controller: function($scope) {
+                $scope.closeThisDialog = function() {
+                    ngDialog.close(); //关闭弹窗
+                };
+            }
+        })
+    }
 });
